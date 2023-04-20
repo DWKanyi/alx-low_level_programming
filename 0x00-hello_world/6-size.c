f7653865ab581d08249237c8ea6c1ea6c139e38d
DWKanyi <dharma_wanjiku@hotmail.com>
2023-04-20 23:16:41 +0300

    C programming starts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * return - always 0 (success)
+*/
+int main(void)
+{
+	printf("Size of a char:\n", sizeof(char));
+	printf("Size of an int:\n", sizeof(int));
+	printf("Size of a long int:\n", sizeof(long int));
+	printf("Size of a long long int:\n", sizeof(long long int));
+	printf("Size of a float:\n", sizeof(float));
+	return (0);
+}
