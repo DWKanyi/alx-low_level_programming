e49091509b6b20984dff620427af8f20cb793507
DWKanyi <dharma_wanjiku@hotmail.com>
2023-04-25 21:28:52 +0300

    User defined functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description - A program that outputs _putchar
+ * Return: 0 for success
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return 0;
+}
