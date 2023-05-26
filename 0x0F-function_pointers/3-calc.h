2e4b8922ee1abcac1a853c8536c3048a8c7e2759
DWKanyi <dharma_wanjiku@hotmail.com>
2023-05-26 11:49:12 +0300

    Header file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -14,11 +14,11 @@ typedef struct op
     int (*f)(int a, int b);
 } op_t;
 
-int op_add(int a, int b); /* Addition of a and b */
-int op_sub(int a, int b); /* Subtraction of a and b */
-int op_mul(int a, int b); /* Multiplication of a and b */
-int op_div(int a, int b); /* Division of a and b */
-int op_mod(int a, int b); /* Modulo of a and b */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
 #endif /* CALC_H */
